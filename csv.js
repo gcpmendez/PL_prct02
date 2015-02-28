ab48759ef42458f0d3f42ea626c3af9d293bb78a
gcpmendez <gcpmendez@gmail.com>
2015-02-28 01:56:44 +0000

    index.html updated

--- csv.js
+++ csv.js
@@ -1,5 +1,3 @@
-
-
 // See http://en.wikipedia.org/wiki/Comma-separated_values
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 
@@ -7,22 +5,19 @@ $(document).ready(function() {
    $("button").click(function() {
      calculate();
    });
- });
+ });// utiliza un objeto de jquery para gestionar el click y la llamada a la funcion del javascript
 
 function calculate() {
   var result;
-  var original       = document.getElementById("original");
+  var original = document.getElementById("original");
   var temp = original.value;
-  var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
+  var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;   // Expresion regular que analizar� nuestro string de entrada al programa
   var lines = temp.split(/\n+\s*/);
-  var commonLength = NaN;
+  var commonLength = NaN; // la longitud de la linea ( longitud comun ) 
   var r = [];
-  // Template using underscore
-  var row = "<%% _.each(items, function(name) { %>"     +
-            "                    <td><%%= name %></td>" +
-            "              <%% }); %>";
-
-  if (window.localStorage) localStorage.original  = temp;
+  
+  if (typeof(Storage !== "undefined"))
+    if (window.localStorage) localStorage.original  = temp;
   
   for(var t in lines) {
     var temp = lines[t];
@@ -43,14 +38,14 @@ function calculate() {
         var removecomma = m[i].replace(/,\s*$/,'');
         var remove1stquote = removecomma.replace(/^\s*"/,'');
         var removelastquote = remove1stquote.replace(/"\s*$/,'');
-        var removeescapedquotes = removelastquote.replace(/\"/,'"');
+        var removeescapedquotes = removelastquote.replace(/\\"/,'"');   // Nos deja el string sin impurezas
         result.push(removeescapedquotes);
       }
       var tr = error? '<tr class="error">' : '<tr>';
-      r.push(tr+_.template(row, {items : result})+"</tr>");
+      r.push(tr+_.template(tm.innerHTML, {items : result})+"</tr>");
     }
     else {
-      alert('ERROR! row '+temp+' does not look as legal CSV');
+      alert("ERROR! \nAlguna de las lineas del string introducido no es valido.");
       error = true;
     }
   }
@@ -58,11 +53,15 @@ function calculate() {
   r.push('</table>');
   //alert(r.join('\n')); // debug
   resultado.innerHTML = r.join('\n');
+  
+  if(error == true)
+    mal.innerHTML = "";
 }
 
-window.onload = function() {
-  // If the browser supports localStorage and we have some stored data
-  if (window.localStorage && localStorage.original) {
-    document.getElementById("original").value = localStorage.original;
-  }
+if (typeof(Storage !== "undefined")){
+  window.onload = function() {
+    // If the browser supports localStorage and we have some stored data
+    if (window.localStorage && localStorage.original)
+      document.getElementById("original").value = localStorage.original;
+  }; // Introducimos el valor que habiamos puesto antiguamente en el elemento de entrada
 };
