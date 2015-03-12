d045b0c3c1cbae11b2d96ede5db92212e8416401
gcpmendez <gcpmendez@gmail.com>
2015-03-12 09:47:26 +0000

    tests.js updated

---
+++ tests/tests.js
@@ -0,0 +1,21 @@
+var assert = chai.assert;
+var tabla_ej = '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>4</td>                                  <td> 6</td>                                  <td> e </td>              </tr>\n<tr>                    <td>8</td>                                  <td> t</td>                                  <td> 2</td>              </tr>\n</tbody></table>';
+
+suite('CSV', function() {
+    setup(function(){
+        if (typeof __html__ !== 'undefined') {
+            document.body.innerHTML = __html__['tests/index.html'];
+            original = document.getElementById('original');
+            finaltable = document.getElementById('finaltable');
+        }
+    });
+    test('Prueba de creación de la tabla', function(){
+      original.value = "4, 6, e \n 8, t, 2";
+      calculate();
+      assert.deepEqual(finaltable.innerHTML, tabla_ej);
+    });
+    test('Prueba del localStorage', function(){
+      if (window.localStorage)
+      assert.deepEqual(localStorage.original, '4, 6, e \n 8, t, 2');
+    });
+});
