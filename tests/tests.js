6c2a65f9e436a6bbe8eaf6c45bdbdb2924ed0029
gcpmendez <gcpmendez@gmail.com>
2015-03-12 10:00:42 +0000

    tests.js updated

--- tests/tests.js
+++ tests/tests.js
@@ -1,21 +1,55 @@
 var assert = chai.assert;
-var tabla_ej = '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>4</td>                                  <td> 6</td>                                  <td> e </td>              </tr>\n<tr>                    <td>8</td>                                  <td> t</td>                                  <td> 2</td>              </tr>\n</tbody></table>';
 
-suite('CSV', function() {
-    setup(function(){
-        if (typeof __html__ !== 'undefined') {
-            document.body.innerHTML = __html__['tests/index.html'];
-            original = document.getElementById('original');
-            finaltable = document.getElementById('resultado');
-        }
-    });
-    test('Prueba de creación de la tabla', function(){
-      original.value = "4, 6, e \n 8, t, 2";
-      calculate();
-      assert.deepEqual(finaltable.innerHTML, tabla_ej);
-    });
-    test('Prueba del localStorage', function(){
-      if (window.localStorage)
-      assert.deepEqual(localStorage.original, '4, 6, e \n 8, t, 2');
-    });
+suite('Comma Separated Values', function() {
+
+  setup(function(){
+    if (typeof __html__ !== 'undefined') {
+      document.body.innerHTML = __html__['tests/index.html'];
+      original = document.getElementById('original');
+      converted = document.getElementById('resultado');
+    }
+  });
+  
+  test('click button is calling calculate()', function() {
+  	original.value = "25";
+  	$("button").trigger("click");
+  	assert.deepEqual(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>25</td>              </tr>\n</tbody></table>');
+  });
+  
+  test('alert is getting showed on error', function() {
+  	original.value = "";
+  	window.alert = function() {};
+    var _savedAlert = window.alert; 
+		try{
+			var spy = sinon.spy(window, 'alert');
+			calculate();
+			sinon.assert.called(spy);
+		}finally{ 
+			window.alert = _savedAlert; 
+		}
+  });
+  
+  test('localStorage working', function() {
+  	original.value = "25";
+    calculate();
+    if (window.localStorage) assert.deepEqual(localStorage.original, original.value);
+  });
+
+  test('calculate function working on 1 value', function() {
+    original.value = "32F";
+    calculate();
+    assert.deepEqual(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>32F</td>              </tr>\n</tbody></table>');
+  });
+  
+  test('caculate function working on 3 values', function() {
+    original.value = "32, 25, DD";
+    calculate();
+    assert.deepEqual(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>32</td>                                  <td> 25</td>                                  <td> DD</td>              </tr>\n</tbody></table>');
+  });
+  
+  test('caculate function working on values in different lines', function() {
+    original.value = "32\n25, DD";
+    calculate();
+    assert.deepEqual(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>32</td>              </tr>\n<tr class="error">                    <td>25</td>                                  <td> DD</td>              </tr>\n</tbody></table>');
+  });
 });
